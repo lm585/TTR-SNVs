9c9a5dd4adea0ddda98f42b7d112fbcacda69c56
lm585 <lm585@users.noreply.github.com>
2017-08-30 11:50:20 -0400

    C++ count number of fields delimited by a SINGLE
    tab
    \t\ta\t              FOUR fields

---
+++ table-numOfFields.C
@@ -0,0 +1,197 @@
+/*
+
+100     Chr1    21089763        21089921        Chr1    21090056        21090218        3       F,      R,      INSERTION       380.333333333333        UNBAL   478    13       10      101,204,3,34,41,48,89,96,97,G13,
+
+output 
+Chr1 21089921 21090056  INSERTION     0	1	0 0 0 0 0 1 1 1 0 1 0 ....
+
+126 accessions, if presented 1; otherwise 0;
+
+
+vector<string> accessionVect;      //input file containing 126 accession names
+construct map<string, bool> acc;
+output header
+for each line in conserv.txt file  //representative conserv.txt file
+  parse line into fields
+  call getAccPresent()
+  output selected fields
+  for each elem in accessionVector //output file
+    if(acc[elem] == true)
+      print "\t" 1
+    else
+      print "\t" 0
+
+
+getAccPresent( map<string, bool> & acc, string col1, string field)
+{
+ set each member of acc to be false
+ if(acc.count(col1) == 0)  //a new accession not in the input file
+   cerr << new accession 
+   exit(1)
+ acc[col1] = 'true';
+ parse field into a list
+ for each member m in the list
+   if(acc.count(col1) == 0)
+     cerr << new accession
+     exit(1)
+   acc[m] = 'true'
+}
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+void readFile(const char * argv, ifstream & input);
+void writeFile(const char * argv, ofstream & output);
+void getFieldContent(vector<string> & fields, char del, string line);
+void getFieldContent2(vector<string> & fields, string del, string line);
+
+int main(int argc, char *argv[])
+{
+ if(argc != 2)
+ {
+  cerr << argv[0] << "  tab-delimited-file" << endl;
+  cerr << "The program outputs the number of fields delimited by A SINGLE tab" << endl;
+  cerr << "It also outputs number of fields that are not null" << endl;
+  cerr << "line_1	53(fields)	30(fields NOT NULL" << endl;
+  return 1;
+ }
+
+ ifstream input;
+ ofstream output;
+ string line;
+ vector<string> lineFields;
+ int k = 0;
+
+ readFile(argv[1], input);
+ getline(input, line);
+ while(!input.eof())
+ {
+  int a, b;
+  getFieldContent(lineFields, '\t', line);
+  a = lineFields.size();
+  b = 0;
+  k++;
+  for(int i = 0; i < a; i++)
+  {
+   if(lineFields[i] != "")
+   {
+    b++;
+   }
+  }
+  cout << k << '\t' << a << '\t' << b << endl;
+  getline(input, line);
+ }
+ input.close();
+ 
+ //writeFile(argv[], output);
+ //output.close();
+ return 0;
+}
+
+void readFile(const char * argv, ifstream & input)
+{
+ input.open(argv, ios::in);
+ if(!input)
+ {
+  cerr << argv << " can not be opened for reading.\n";
+  exit(1);
+ }
+}
+
+void writeFile(const char * argv, ofstream & output)
+{
+ output.open(argv, ios::out);
+ if(!output)
+ {
+  cerr << argv << " can not be opened for writing.\n";
+  exit(1);
+ }
+}
+
+void getFieldContent(vector<string> & fields, char del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del == line[i])
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   str = line.substr(pos[i] + 1, len);
+   fields.push_back(str);
+  }
+  size = pos.size();
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+void getFieldContent2(vector<string> & fields, string del, string line)
+{
+ vector<int> pos;
+ string str;
+ int len, size;
+
+ fields.clear();
+ for(int i = 0; i < line.length(); i++)
+ {
+  if(del.find(line[i]) != string::npos)
+    pos.push_back(i);
+ }
+ if(pos.size() > 0)
+ {
+  len = pos[0];
+  str = line.substr(0, len);
+  if(len > 0)
+    fields.push_back(str);
+  for(int i = 0; i < pos.size() - 1; i++)
+  {
+   len = pos[i+1] - pos[i] - 1;
+   if(len > 0)
+   {
+    str = line.substr(pos[i] + 1, len);
+    fields.push_back(str);
+   }
+  }
+  size = pos.size();
+  if(pos[size-1] < line.length() - 1) //not at the end of line
+  {
+   str = line.substr(pos[size-1] + 1);
+   fields.push_back(str);
+  }
+ }
+ else
+ {
+  fields.push_back(line);
+ }
+}
+
+
